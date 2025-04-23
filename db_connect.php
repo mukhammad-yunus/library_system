137dbaf9a48c99f7a2f46e04de700174cd6402ad
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-04-23 12:00:39 +0200

    sensitive data access bug fixed

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,10 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $host = $_ENV['DB_HOST'];
 $username = $_ENV['DB_USER'];
 $password =$_ENV['DB_PASS'];
