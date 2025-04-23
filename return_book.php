be368a7289d7beb8d542ba8646f5cee6dfb89c52
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-04-23 11:27:50 +0200

    return book logic

---
+++ return_book.php
@@ -0,0 +1,19 @@
+<?php
+include "db_connect";
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  $loan_id = $_POST['loan_id'];
+  $return_date = $_POST['return_date'];
+
+  if(!empty($loan_id) && !empty($return_date)){
+    $sql = "INSERT INTO return_book (loan_id, return_date) VALUES ($loan_id, '$return_date')";
+
+    if(mysqli_query($conn, $sql)){
+      echo "Book returned successfully!";
+    } else{
+      echo "Error: " . mysqli_error($conn);
+    }
+  }
+}
+
+mysqli_close($conn);
